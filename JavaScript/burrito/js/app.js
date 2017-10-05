e4c8a749eb3cddd9a21e169dc73aa385f51ee272
Azenwrath <dana.stubkjaer@gmail.com>
2017-10-05 14:06:55 -0700

    Worked on burrito lab.

--- JavaScript/burrito/js/app.js
+++ JavaScript/burrito/js/app.js
@@ -1,21 +1,49 @@
 "use strict";
 $('.ui.checkbox').checkbox()
+var serialarr;
+var serialized;
+var item;
+var burritoList= [];
+var burritoTranslator = {
+    wheat: "Wheat Tortilla",
+    white: "White Tortilla",
+    spinach: "Spinach Tortilla",
+    corn: "Corn Tortilla",
+    carnitas: "Carnitas",
+    chicken: "Chicken",
+    sofritas: "Sofritas",
+    beans: "Beans",
+    cheese: "Cheese",
+    salsa: "Salsa",
+    'sour cream': "Sour Cream",
+    guacamole: "Guacamole",
+    "scrambled-egg": "Scrambled egg",
+    potatoes: "Potatoes",
+    "sun-dried-tomatoes": "Sun-dried Tomatoes",
+    olives: "Olives",
+    "sauteed-mushrooms": "Sauteed Mushrooms",
+    "sauteed-onions": "Sauteed Onions",
+    jalapenos: "Jalapenos"
+
+
+
+};
+
 
 
 
 // $('[name="extra-ingredients"]').change(function(e) {
 $('.ui.form').change(function () {
-
     var serialized = decodeURI($(this).serialize());
-    console.log(serialized);
     var serialarr = serialized.split('&');
+    burritoList = []; // Clear burritoList to prevent exponential burritos
     for (item in serialarr) {
         // console.log("Item: " + serialarr[item]);
-        var twothings = serialarr[item].split('=');
-        console.log("Parsed: " + twothings);
+        burritoList.push(serialarr[item].split('='));
     }
-    console.log (serialarr);
-    $('#total_cost').html("Test");
+    console.log ("Passing burritoList: " + burritoList);
+    updateIngredients(burritoList);
+
 });
 //
 // var burrito = {
@@ -28,7 +56,28 @@ $('.ui.form').change(function () {
 // console.log(ingredients[0].name());
 
 function updateIngredients(ingredientlist) {
+    var firstIngredient;
+    var addons = 0;
+    var delivery = 0;
+    $('#ingredients > ul > li').remove();
     for (item in ingredientlist) {
+        firstIngredient=ingredientlist[item][0];
+        if (firstIngredient === "included-ingredients"||firstIngredient === "tortilla" || firstIngredient === "meat") {
+            console.log("Ingredient: " + ingredientlist[item][1]);
+            $('#ingredients > ul').append('<li class="item">'+ burritoTranslator[ingredientlist[item][1]] + '</li>');
+        } else if (firstIngredient === "extra-ingredients") {
+            console.log("Ingredient: " + ingredientlist[item][1]);
+            $('#ingredients > ul').append('<li class="item">'+ burritoTranslator[ingredientlist[item][1]] + '</li>');
+            addons++;
+        } else if (firstIngredient === "delivery" && ingredientlist[item][1] == "delivery") {
+            console.log("I found delivery status");
+            delivery = 5;
 
+        }
+
+
+        $('#total_cost').html("Total: $" + Number(6 + delivery + (addons * .5)).toFixed(2));
     }
 }
+
+
--- JavaScript/burrito/js/thanks.js
+++ JavaScript/burrito/js/thanks.js
@@ -1,121 +1,74 @@
-/**
- * Update a UI widget with the list of ingredients being ordered.
- * @desc Update the DOM element passed in to the `output` argument with HTML
- * structure in the following way:
- * <ul class="ui relaxed list">
- *     <li class="item">White Tortilla</li>
- *     <li class="item">Beans</li>
- *     <li class="item">Cheese</li>
- *     <li class="item">Salsa</li>
- *     <li class="item">Sour Cream</li>
- * </ul>
- * @param {DOM element} output Reference to the <ul></ul> DOM element in which
- *     to display the order details as <li></li> tags
- * @param {Object} costs Object literal containing data about the costs of
- * different items, formatted as follows:
- * {
- *     String: Number.<amount>,
- *     String: Number.<amount>,
- *     ...
- *     String: Number.<amount>
- * }
- * Where the key is a string that matches the `name` attribute of a field or
- * fields (i.e. a set of checkboxes) and the value is the amount that thing
- * or each of those things cost.
- * @return {void}
- */
-define(['./total-updater'], function (totalUpdater) {
-    function thanks(output, costs) {
-        var ingredients = [
-            'tortilla',
-            'meat',
-            'included-ingredients',
-            'extra-ingredients'
-        ];
+"use strict";
+var burritoTranslator = {
+    wheat: "Wheat Tortilla",
+    white: "White Tortilla",
+    spinach: "Spinach Tortilla",
+    corn: "Corn Tortilla",
+    carnitas: "Carnitas",
+    chicken: "Chicken",
+    sofritas: "Sofritas",
+    beans: "Beans",
+    cheese: "Cheese",
+    salsa: "Salsa",
+    'sour cream': "Sour Cream",
+    guacamole: "Guacamole",
+    "scrambled-egg": "Scrambled egg",
+    potatoes: "Potatoes",
+    "sun-dried-tomatoes": "Sun-dried Tomatoes",
+    olives: "Olives",
+    "sauteed-mushrooms": "Sauteed Mushrooms",
+    "sauteed-onions": "Sauteed Onions",
+    jalapenos: "Jalapenos"
 
-        var humanReadable = { // TODO expose this from a module to make things more DRY
-            'white': 'White Flour Tortilla', 'wheat': 'Wheat Flour Tortilla',
-            'spinach': 'Spinach Tortilla', 'corn': 'Corn Tortilla (gluten-free)',
-            'carnitas': 'Carnitas', 'chicken': 'Chicken', 'sofritas': 'Sofritas (tofu)',
-            'beans': 'Beans', 'cheese': 'Cheese', 'salsa': 'Salsa', 'sour cream': 'Sour Cream',
-            'guacamole': 'Guacamole', 'scrambled-egg': 'Scrambled Egg', 'potatoes': 'Home Fried Potatoes',
-            'sun-dried-tomatoes': 'Sun-dried Tomatoes', 'olives': 'Olives',
-            'sauteed-mushrooms': 'Sautéed Mushrooms', 'sauteed-onions': 'Sautéed Onions',
-            'jalapenos': 'Jalapeño Peppers'
-        };
 
-        // TODO expose this from a module
-        // parse the browser's query string
-        // based on: http://stackoverflow.com/a/3855394/11577
-        // (but changed to handle multiple instances any particular name)
-        var qs = (function(nvps) { // nvps == name value pairs
-            if (nvps === '') {
-                return null;
-            }
-            var rzlt = {};
-            var rgxp = /\+/g;
-            nvps.forEach(function (nvp) {
-                var pair = nvp.split('=', 2); // limit to 2 items
-                if (pair.length > 1) {
-                    // if the key already exists, make the values into an array
-                    if (rzlt[pair[0]]) {
-                        // if it's ready an array, just add the new value
-                        if (Array.isArray(rzlt[pair[0]])) {
-                            rzlt[pair[0]].push(pair[1]);
-                        }
-                        // convert the value into an array and add the new value
-                        else {
-                            rzlt[pair[0]] = [
-                                rzlt[pair[0]],
-                                pair[1]
-                            ];
-                        }
-                    }
-                    // create a new key
-                    else {
-                        rzlt[pair[0]] = decodeURIComponent(pair[1].replace(rgxp, ' '));
-                    }
-                }
-            });
-            return rzlt;
-        })(window.location.search.substr(1).split('&'));
 
-        var lis  = [];
-        for (var key in qs) {
-            if (ingredients.indexOf(key) > -1) {
-                if (Array.isArray(qs[key])) {
-                    qs[key].forEach(function (value) {
-                        lis.push(`<li class="item">${humanReadable[value]}</li>`);
-                    });
-                }
-                else {
-                    lis.push(`<li class="item">${humanReadable[qs[key]]}</li>`);
-                }
-            }
-        }
+};
+var burritoList = [];
+var item;
+console.log(location.search);
 
-        var costs            = { 'extra-ingredients': 0.5, 'delivery': 5 }; // TODO expose this from a module to make things more DRY
-        var numberOfExtras   = qs['extra-ingredients'] ? qs['extra-ingredients'].length : 0;
-        var isBeingDelivered = qs['delivery'] === 'delivery';
+$( document ).ready(updateIngredients(decodeURI(location.search)));
 
-        var totalCost        = totalUpdater.calculateCost(costs, numberOfExtras, isBeingDelivered);
-        var deliveryOption   = isBeingDelivered ? 'delivery' : 'in-store pick-up';
 
-        if (lis.length > 0) {
-            var html = `
-                <ul class="ui relaxed list">
-                    ${lis.join('')}
-                </ul>
-                <p>
-                    Your order is set for <strong>${deliveryOption}.</strong>
-                </p>
-                <h3 class="ui block header">
-                    Total Cost: $${totalCost}
-                </h3>
-            `;
-
-            output.innerHTML = html;
-        }
+function updateIngredients(ingredientlist) {
+    var firstIngredient;
+    var addons = 0;
+    var delivery = 0;
+    var serialarr = ingredientlist.substring(ingredientlist.indexOf("?") + 1).split('&');
+    burritoList = []; // Clear burritoList to prevent exponential burritos
+    for (item in serialarr) {
+        // console.log("Item: " + serialarr[item]);
+        burritoList.push(serialarr[item].split('='));
     }
-    return thanks;
-});
+    ingredientlist = burritoList;
+
+
+
+
+
+
+
+    console.log("Started update");
+
+    $('#order-details').remove("h4");
+    $('#order-details').append('<ul id="orderoutput"></ul>');
+    for (item in ingredientlist) {
+        firstIngredient=ingredientlist[item][0];
+        console.log(ingredientlist[item][0]);
+        if (firstIngredient === "included-ingredients"||firstIngredient === "tortilla" || firstIngredient === "meat") {
+            console.log("Ingredient: " + ingredientlist[item][1]);
+            $('#orderoutput').append('<li class="item">'+ burritoTranslator[ingredientlist[item][1]] + '</li>');
+        } else if (firstIngredient === "extra-ingredients") {
+            console.log("Ingredient: " + ingredientlist[item][1]);
+            $('#orderoutput').append('<li class="item">'+ burritoTranslator[ingredientlist[item][1]] + '</li>');
+            addons++;
+        } else if (firstIngredient === "delivery" && ingredientlist[item][1] == "delivery") {
+            console.log("I found delivery status");
+            delivery = 5;
+
+        }
+
+
+        $('#total_cost').html("Total: $" + Number(6 + delivery + (addons * .5)).toFixed(2));
+    }
+}
