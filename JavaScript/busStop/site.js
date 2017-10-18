116e0bb0919bfe5bf7a43e5a854eb6defc391af2
Azenwrath <dana.stubkjaer@gmail.com>
2017-10-18 16:29:34 -0700

    Additional Bus Stop fiddling to working prototype

--- JavaScript/busStop/site.js
+++ JavaScript/busStop/site.js
@@ -1,5 +1,12 @@
 "use strict";
-let myAPPID;
+let myAPPID; // Stores the API Key for trimet.
+
+
+fetchKey(); //Makes AJAX call to local file for secret API key and then calls getCurrentPosition
+//and getTrimetStopsByLocation to start script.
+
+
+
 
 
 function fetchKey () {$.ajax({
@@ -11,26 +18,53 @@ function fetchKey () {$.ajax({
     }
 })}
 
+/*
+This function queries the Trimet API for stop locations. API key is filled from global.
 
+
+Arg: location object pulled from browser
+
+*/
 
 function getTrimetStopsByLocation (data) {
-    console.log(data);
-    console.log("Lat: " + data.coords.latitude);
-    console.log("Lat: " + data.coords.longitude);
+
     let trimetdata = {
         appID: myAPPID,
         json: true,
-        ll: data.coords.latitude + ',' + data.coords.longitude
+        ll:'45.496673,-122.622570' //data.coords.latitude + ',' + data.coords.longitude
     };
-    console.log(trimetdata);
+
     $.ajax({
         url: 'http://developer.trimet.org/ws/V1/stops',
         type: 'GET',
         data: trimetdata,
-        success: function (ee) {
-            console.log(ee)
-        },
+        success: parseTrimetResults
     })
 }
 
-fetchKey();
+function parseTrimetResults (ee) {
+            const results = ee.resultSet.location;
+            console.log("Results: " + results);
+            console.table(results);
+            console.table(results[0]);
+            results.forEach(function (i) {
+                console.log("Desc: " + i.desc);
+                console.log("Dir: " + i.dir);
+                console.log("Lat: " + i.lat);
+                console.log("Long: " + i.lng);
+                console.log("Locid: " + i.locid);
+                populateStopList(i);
+            })
+
+}
+
+function populateStopList (stop) {
+    const listNode = document.querySelector('#stoplist');
+    const newNode = document.createElement('li');
+    const newContent = document.createTextNode(stop.desc);
+    listNode.appendChild(newNode);
+    newNode.appendChild(newContent);
+}
+
+
+
